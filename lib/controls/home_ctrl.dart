915daf217ce5a8da29a6c956f76dbc68efe7c5c3
Razzorback225 <47690391+Razzorback225@users.noreply.github.com>
2022-07-20 19:30:41 +0200

    Renamed controller

--- lib/controls/home_ctrl.dart
+++ lib/controls/home_ctrl.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
-import 'package:hhm_demo_app/tools/api_fetcher.dart';
 import 'package:hhm_demo_app/models/country_data.dart';
 
-Completer<GoogleMapController> c_controller = Completer();
+Completer<GoogleMapController> cController = Completer();
 BuildContext? ctxt;
 Set<Marker> markers = Set<Marker>();
 List<CountryData> countries = List<CountryData>.empty(growable: true);
